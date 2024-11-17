cbe244334156cc3760a336d16e598f2d066b9ec3
s0k1y <2104542143@qq.com>
2024-11-17 11:49:22 +0800

    Site updated: 2024-11-17 11:49:22

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("花开了", "风起了", "凤凰飞了");
+        ("WEB", "MISC", "Crypto");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
